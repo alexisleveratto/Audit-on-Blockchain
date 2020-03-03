5561bd0a03b7148464d89ae33ae152a628f91f78
alexis-x-leveratto <alexis.x.leveratto@pwc.com>
2020-03-03 16:08:57 -0300

    Add first Templates

---
+++ src/.flaskenv
@@ -0,0 +1,1 @@
+FLASK_APP=blockchainaudit.py
---
+++ src/app/routes.py
@@ -0,0 +1,20 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username' : 'aleveratto001'}
+    title = 'Menu Principal'
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    # return render_template('index.html', title=title, user=user)
+    return render_template('index.html', title=title, user=user, posts=posts)
