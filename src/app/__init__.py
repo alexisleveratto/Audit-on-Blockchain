fb5aef2b78ecdc53b9c2ccbbf3f219747ae45df7
alexis-x-leveratto <alexis.x.leveratto@pwc.com>
2020-03-03 16:17:04 -0300

    Config secret key

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,5 +1,8 @@
+from config import Config
 from flask import Flask
 
-app = Flask(__name__)
 
+app = Flask(__name__)
+app.config.from_object(Config)
+print(app.config["SECRET_KEY"])
 from app import routes
--- src/app/routes.py
+++ src/app/routes.py
@@ -1,20 +1,15 @@
 from app import app
 from flask import render_template
 
-@app.route('/')
-@app.route('/index')
+
+@app.route("/")
+@app.route("/index")
 def index():
-    user = {'username' : 'aleveratto001'}
-    title = 'Menu Principal'
+    user = {"username": "aleveratto001"}
+    title = "Menu Principal"
     posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
+        {"author": {"username": "John"}, "body": "Beautiful day in Portland!"},
+        {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!"},
     ]
     # return render_template('index.html', title=title, user=user)
-    return render_template('index.html', title=title, user=user, posts=posts)
+    return render_template("index.html", title=title, user=user, posts=posts)
