4de41b1b0f706420a8ab326677f5fecd55d86064
alexis-x-leveratto <alexis.x.leveratto@pwc.com>
2020-03-04 15:16:05 -0300

    Register new User

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,5 +1,6 @@
 from config import Config
 from flask import Flask
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
@@ -7,6 +8,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+login = LoginManager(app)
+login.login_view = "login"
 migrate = Migrate(app, db)
 
 from app import routes, models
--- src/app/forms.py
+++ src/app/forms.py
@@ -1,6 +1,7 @@
+from app.models import User
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +9,23 @@ class LoginForm(FlaskForm):
     password = PasswordField("Contraseña", validators=[DataRequired()])
     remember_me = BooleanField("Recuerdame")
     submit = SubmitField("Iniciar Sesion")
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Usuario", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Contraseña", validators=[DataRequired()])
+    password2 = PasswordField(
+        "Repita Contraseña", validators=[DataRequired(), EqualTo("password")]
+    )
+    submit = SubmitField("Registrarse")
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError("Utilice otro nombre de usuario.")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError("Utilice otro email.")
