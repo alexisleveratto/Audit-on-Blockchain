85b5b78842aeca587417f45d73ac9990b3523b4a
alexis-x-leveratto <alexis.x.leveratto@pwc.com>
2020-03-04 15:25:48 -0300

    Add routes

--- src/app/routes.py
+++ src/app/routes.py
@@ -1,5 +1,5 @@
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 from flask import flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required, login_user, logout_user
@@ -41,3 +41,17 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("index"))
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Registrado')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Registro', form=form)
