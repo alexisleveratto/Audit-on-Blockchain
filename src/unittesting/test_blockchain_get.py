e820b470a691eef96a7160dad0197975af2c0d5d
alexis-x-leveratto <alexis.x.leveratto@pwc.com>
2020-09-09 20:47:04 -0300

    start unit testing

--- src/unittesting/test_blockchain_get.py
+++ src/unittesting/test_blockchain_get.py
@@ -1,2 +1,24 @@
 import pytest
 import requests
+
+from app.models import User
+
+def test_login_audit_ok():
+    test_audit_username = 'Audit'
+    test_audit_password = 'audit'
+    
+    test_audit = User.query.filter_by(username=test_audit_username).first()
+    assert test_audit.check_password(test_audit_password)
+
+def test_login_audit_fail_username():
+    test_audit_username = 'Not-Audit'
+
+    test_audit = User.query.filter_by(username=test_audit_username).first()
+    assert not(test_audit)
+
+def test_login_audit_fail_password():
+    test_audit_username = 'Audit'
+    test_audit_password = 'not-password'
+
+    test_audit = User.query.filter_by(username=test_audit_username).first()
+    assert not(test_audit.check_password(test_audit_password))
